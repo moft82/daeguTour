a2280210225a40f7a4a8ca17d654b5de5dab684c
moft82 <34787260+moft82@users.noreply.github.com>
2021-12-16 15:03:11 +0900

    code refactor
    code refactor

--- app/src/main/java/com/example/daegutour/CsvReader.kt
+++ app/src/main/java/com/example/daegutour/CsvReader.kt
@@ -7,15 +7,14 @@ import java.io.InputStreamReader
 import java.nio.charset.Charset
 
 class CsvReader {
-
-    fun readCSVWifi(ins : InputStream): List<Map<String, String>> {
+    fun readFacilityCSV(ins: InputStream): List<Map<String, String>> {
         try {
             var reader: BufferedReader? = null
             reader = BufferedReader(InputStreamReader(ins, Charset.forName("utf8")))
             val data = mutableListOf<Map<String, String>>()
             var line = reader.readLine()
 
-//            Column line pass
+            // Column line pass
             line = reader.readLine()
             while (line != null) {
                 val tokens = line.split('|')
@@ -24,116 +23,27 @@ class CsvReader {
                 dataMap["name"] = tokens[0]
                 dataMap["latitude"] = tokens[1]
                 dataMap["longitude"] = tokens[2]
-                dataMap["management"] = tokens[3]
-                dataMap["phone_number"] = tokens[4]
-                dataMap["adress"] = tokens[5]
-                dataMap["service"] = tokens[6]
-                dataMap["location"] = tokens[7]
-                dataMap["SSID"] = tokens[8]
                 data.add(dataMap)
                 line = reader.readLine()
             }
-            Log.d("csv data read", "Wifi CSV Data Read Success")
-            Log.d("csv data", data[0]["latitude"].toString())
+            Log.d("csv read", "Facility CSV")
             return data
         } catch (e: Exception) {
-            Log.d("error", "CSV Data Read Error")
-            Log.d("error", e.toString())
+            Log.e("csv read", e.toString())
             return mutableListOf()
-        }
-    }
-
-    fun readCSVToilet(ins : InputStream): List<Map<String, String>> {
-        try {
-            var reader: BufferedReader? = null
-            reader = BufferedReader(InputStreamReader(ins, Charset.forName("utf8")))
-            val data = mutableListOf<Map<String, String>>()
-            var line = reader.readLine()
-
-//            Column line pass
-            line = reader.readLine()
-            while (line != null) {
-                var tokens = line.split('|')
-                val dataMap = mutableMapOf<String, String>()
-
-                dataMap["name"] = tokens[0]
-                dataMap["latitude"] = tokens[1]
-                dataMap["longitude"] = tokens[2]
-                dataMap["management"] = tokens[3]
-                dataMap["phone_number"] = tokens[4]
-                dataMap["adress"] = tokens[5]
-                dataMap["operate_time"] = tokens[6]
-                dataMap["type"] = tokens[7]
-                dataMap["uni_sex"] = tokens[8]
-                data.add(dataMap)
-                line = reader.readLine()
-            }
-            Log.d("csv data read", "Toilet CSV Data Read Success")
-            return data
-        } catch (e: Exception) {
-            Log.d("error", "Toilet CSV Data Read Error")
-            Log.d("error", e.toString())
-
-            return mutableListOf<Map<String, String>>()
-        }
-
-    }
-
-    fun readCSVTourguide(ins: InputStream): List<Map<String, String>> {
-        try {
-            var reader: BufferedReader? = null
-            reader = BufferedReader(InputStreamReader(ins, Charset.forName("utf8")))
-            val data = mutableListOf<Map<String, String>>()
-            var line = reader.readLine()
-
-//            Column line pass
-            line = reader.readLine()
-            while (line != null) {
-                var tokens = line.split('|')
-                val dataMap = mutableMapOf<String, String>()
-
-                dataMap["name"] = tokens[0]
-                dataMap["latitude"] = tokens[1]
-                dataMap["longitude"] = tokens[2]
-                dataMap["management"] = tokens[3]
-                dataMap["phone_number"] = tokens[4]
-                dataMap["adress"] = tokens[5]
-                dataMap["service"] = tokens[6]
-                dataMap["location"] = tokens[7]
-                dataMap["operate_time(summer)"] = tokens[8]
-                dataMap["operate_time(winter)"] = tokens[8]
-                dataMap["holiday"] = tokens[8]
-                dataMap["english"] = tokens[8]
-                dataMap["japanese"] = tokens[8]
-                dataMap["chinese"] = tokens[8]
-                dataMap["desc"] = tokens[8]
-                dataMap["page"] = tokens[8]
-
-                data.add(dataMap)
-                line = reader.readLine()
-            }
-            Log.d("csv data read", "Tour Guide CSV Data Read Success")
-            return data
-        } catch (e: Exception) {
-            Log.d("error", "Tour Guide CSV Data Read Error")
-
-            return mutableListOf<Map<String, String>>()
         }
     }
 
     fun readListData(ins: InputStream): List<Map<String, String>> {
         try {
-            var reader: BufferedReader? = null
-            reader = BufferedReader(InputStreamReader(ins, Charset.forName("utf8")))
-            Log.d("dataread","readStart")
+            val reader = BufferedReader(InputStreamReader(ins, Charset.forName("utf8")))
             val data = mutableListOf<Map<String, String>>()
             var line = reader.readLine()
 
-//            Column line pass
+            // Column pass
             line = reader.readLine()
-            Log.d("dataread","Column pass")
             while (line != null) {
-                var tokens = line.split('|')
+                val tokens = line.split('|')
                 val dataMap = mutableMapOf<String, String>()
                 dataMap["img"] = tokens[0]
                 dataMap["name"] = tokens[1]
@@ -150,12 +60,11 @@ class CsvReader {
                 data.add(dataMap)
                 line = reader.readLine()
             }
-            Log.d("csv data read", "Spot CSV Data Read Success")
+            Log.d("csv data", "Spot CSV")
             return data
         } catch (e: Exception) {
-            Log.d("error", "Spot CSV Data Read Error")
-            Log.d("error", e.toString())
-            return mutableListOf<Map<String, String>>()
+            Log.d("csv data", e.toString())
+            return mutableListOf()
         }
     }
 
--- app/src/main/java/com/example/daegutour/Facility.kt
+++ app/src/main/java/com/example/daegutour/Facility.kt
@@ -2,8 +2,6 @@ package com.example.daegutour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import android.widget.Switch
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -14,7 +12,6 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-
 
 class Facility : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
@@ -35,24 +32,23 @@ class Facility : AppCompatActivity(), OnMapReadyCallback {
         val latitude = intent.getFloatExtra("latitude", 35.0f)
         val longitude = intent.getFloatExtra("longitude", 35.0f)
 
-
         mMap = googleMap
-
-        // Data Load
-        val csvReader = CsvReader()
-        val toilet = csvReader.readCSVToilet(resources.openRawResource(R.raw.toilet))
-        val tourGuide = csvReader.readCSVTourguide(resources.openRawResource(R.raw.tour_guide))
-        val wifi = csvReader.readCSVWifi(resources.openRawResource(R.raw.wifi))
-
-        // Add Marker
-        val toiletMarker = addMarker(toilet, BitmapDescriptorFactory.HUE_GREEN, mMap)
-        val tourGuideMarker = addMarker(tourGuide, BitmapDescriptorFactory.HUE_BLUE, mMap)
-        val wifiMarker = addMarker(wifi, BitmapDescriptorFactory.HUE_YELLOW, mMap)
 
         // Add Spot Marker and move Camera
         val spot = LatLng(latitude.toDouble(), longitude.toDouble())
         mMap.addMarker(MarkerOptions().position(spot).title(name))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(spot, 17f))
+
+        // Data Load
+        val csvReader = CsvReader()
+        val toilet = csvReader.readFacilityCSV(resources.openRawResource(R.raw.toilet))
+        val tourGuide = csvReader.readFacilityCSV(resources.openRawResource(R.raw.tour_guide))
+        val wifi = csvReader.readFacilityCSV(resources.openRawResource(R.raw.wifi))
+
+        // Add Marker
+        val toiletMarker = addMarker(toilet, BitmapDescriptorFactory.HUE_GREEN, mMap)
+        val tourGuideMarker = addMarker(tourGuide, BitmapDescriptorFactory.HUE_BLUE, mMap)
+        val wifiMarker = addMarker(wifi, BitmapDescriptorFactory.HUE_YELLOW, mMap)
 
         // Switch Set
         val switchToilet = findViewById<Switch>(R.id.toilet_switch)
@@ -75,9 +71,8 @@ class Facility : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun addMarker(data:List<Map<String, String>>, color: Float, mMap:GoogleMap): MutableList<Marker> {
-        Log.d("Marker", "Start")
         val dataIterator = data.iterator()
-        val markers:MutableList<Marker> = mutableListOf<Marker>()
+        val markers:MutableList<Marker> = mutableListOf()
         while (dataIterator.hasNext()) {
             val tokens = dataIterator.next()
             val position = LatLng(tokens["latitude"]!!.toDouble(), tokens["longitude"]!!.toDouble())
@@ -86,10 +81,9 @@ class Facility : AppCompatActivity(), OnMapReadyCallback {
                 .icon(BitmapDescriptorFactory.defaultMarker(color)))
             )
         }
-        Log.d("Marker", "Done")
         return markers
     }
-    fun markerVisibleSet(markers:List<Marker>, is_visible:Boolean){
+    private fun markerVisibleSet(markers:List<Marker>, is_visible:Boolean){
         val markerIterator = markers.iterator()
         while (markerIterator.hasNext()) {
             val marker = markerIterator.next()
